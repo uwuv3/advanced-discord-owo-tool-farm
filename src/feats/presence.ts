2d498d78f549d30445ded4d5c55faaa6f21987fb
Kyou Izumi <thanhlong0975855842@gmail.com>
2024-12-08 13:22:11 +0700

    Fix presence

--- src/feats/presence.ts
+++ src/feats/presence.ts
@@ -1,22 +1,15 @@
 import { Client, RichPresence } from "discord.js-selfbot-v13";
 
 export const loadPresence = async (client: Client) => {
-    const externalLinks = await RichPresence.getExternal(
-        client,
-        "367827983903490050",
-        "https://i.imgur.com/9wrvM38.png",
-        "https://i.imgur.com/MO5TPzf.png"
-    )
-
     const rpc = new RichPresence(client)
-        .setApplicationId("367827983903490050")
+        .setApplicationId("1205422490969579530")
         .setType("PLAYING")
         .setName("Mirai Kuriyama")
         .setDetails("The day the emperor returns!")
         .setStartTimestamp(client.readyTimestamp ?? Date.now())
-        .setAssetsLargeImage(externalLinks[0].external_asset_path)
+        .setAssetsLargeImage("1312264004382621706")
         .setAssetsLargeText("Oct updates coming, check 'em out!")
-        .setAssetsSmallImage(externalLinks[1].external_asset_path)
+        .setAssetsSmallImage("1306938859552247848")
         .setAssetsSmallText("Kyou Izumi")
         .addButton("GitHub", "https://github.com/Kyou-Izumi/advanced-discord-owo-tool-farm")
         .addButton("YouTube", "https://www.youtube.com/@daongotau")
