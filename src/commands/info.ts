0b1dcb11d9684c0cc23ad2217ef0561bb2489c55
Kyou Izumi <ntt.eternity2k6@gmail.com>
2025-02-04 06:13:21 +0000

    Fix info command

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -5,7 +5,7 @@ const infoCommand: Commands = {
     name: "info",
     description: "Tool Information",
     execute: (agent, message, ...args) => {
-        const status = `Status:__ ${agent.captchaDetected ? agent.paused ? "**PAUSED**" : "**PENDING CAPTCHA**" : "HUNTING"}`;
+        const status = `__Status:__ ${agent.captchaDetected ? agent.paused ? "**PAUSED**" : "**PENDING CAPTCHA**" : "HUNTING"}`;
         const summary = `__Total commands/texts sent:__ **${agent.totalCommands}/${agent.totalTexts}**`;
         const captchaSum = `__Total Captchas Resolved/Unsolved:__ **${agent.totalCaptcha.resolved}/${agent.totalCaptcha.unsolved}**`;
         const uptime = `__Total active time:__ ${timeHandler(agent.readyTimestamp ?? 0, Date.now())}`;
